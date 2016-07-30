19b83dbb8acf26e7436437ecc15dcf931204c708
jetsi <ngwajessi@gmail.com>
2016-07-30 13:52:33 +1200

    gg

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
+<title>Homely </title>
 
-include_once("header.php");
-?>
+<head>
+  <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.min.js" ></script>
+</head>
 
+<nav id="header" class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <ul>
+        <li id="logo"><img id="logo" src="images/logo.png" /> </li>  
+        <div id="right_login">
+         <li>Login</li>
+         <li>Reegister</li>
+        </div>
+        <li></li>
+    </ul>
+  </div>
+</nav>
